3cd520e13f2e8968069bf03797eb8e340bd0d7cf
Nicolasflori <nicolasflori@hotmail.com>
2021-06-09 19:37:09 -0300

    Boton Eliminar terminado, Layout de tabla en carrito, boton Agregar mas articulos funcionando

--- ArticulosWeb/CarritoDeCompras.aspx.cs
+++ ArticulosWeb/CarritoDeCompras.aspx.cs
@@ -15,14 +15,12 @@ namespace ArticulosWeb
         public ItemCarrito elemento;
 
         protected void Page_Load(object sender, EventArgs e)
-
         {
-            elemento = new ItemCarrito();
             carrito = (List<ItemCarrito>)Session["listaCarrito"];
+            elemento = new ItemCarrito();
 
             if (carrito == null)
                 carrito = new List<ItemCarrito>();
-
             if (!IsPostBack)
             {
                 if (Request.QueryString["id"] != null)
@@ -30,14 +28,9 @@ namespace ArticulosWeb
                     if (carrito.Find(x => x.Articulos.ID.ToString() == Request.QueryString["id"]) == null)
                     {
                         List<Articulos> listadoOriginal = (List<Articulos>)Session["listadoArticulos"];
-                        elemento.Cantidad++;
                         elemento.Articulos = listadoOriginal.Find(x => x.ID.ToString() == Request.QueryString["id"]);
                         carrito.Add(elemento);
                     }
-                }
-                else if (Request.QueryString["id"] == "atras")
-                {
-                    Response.Redirect(default);
                 }
                 Repetidor.DataSource = carrito;
 
@@ -49,40 +42,25 @@ namespace ArticulosWeb
         protected void buttonEliminar_Click(object sender, EventArgs e)
         {
             try
-
             {
-
-
-
                 var argument = ((Button)sender).CommandArgument;
-
-
-
                 List<ItemCarrito> carrito = (List<ItemCarrito>)Session["listaCarrito"];
-
                 ItemCarrito elim = carrito.Find(x => x.Articulos.ID.ToString() == argument);
-
                 carrito.Remove(elim);
-
                 Session.Add("listaFavoritos", carrito);
-
                 Repetidor.DataSource = null;
-
                 Repetidor.DataSource = carrito;
-
                 Repetidor.DataBind();
-
-
-
             }
-
             catch (Exception ex)
-
             {
-
                 Response.Redirect("Error.aspx");
-
             }
+        }
+
+        protected void Unnamed_ServerClick(object sender, EventArgs e)
+        {
+            Response.Redirect("Default.aspx");
         }
     }
 }
